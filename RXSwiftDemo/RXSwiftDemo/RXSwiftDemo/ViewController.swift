431a32e115c4e52fe1b57ea44b9554488e195f7e
dengqi <350218638@qq.com>
2019-12-08 21:35:03 +0800

    RxSwift

---
+++ RXSwiftDemo/RXSwiftDemo/RXSwiftDemo/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  RXSwiftDemo
+//
+//  Created by dengqi on 2019/12/1.
+//  Copyright © 2019 DQ. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view
+        _ = btn.rx.tap.subscribe(onNext: { (sender) in
+            
+        }, onError: { (error) in
+            
+        }, onCompleted: {
+            
+        }) {
+            
+        }
+        
+    }
+
+
+}
+
